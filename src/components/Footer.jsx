7602b61c1b2d96d714c3b80acf84402e8dcc56fc
Skia69 <Skiastep@gmail.com>
2021-03-20 13:02:53 +0200

    feat: finish the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => (
+  <footer className="w-full">
+    <ul className="flex justify-around">
+      <li className="px-12">Maryam Abdel Karim</li>
+      <li className="px-12">Mhamad Ibrahim</li>
+      <li className="px-12">Charbel Tarrazi</li>
+    </ul>
+  </footer>
+);
