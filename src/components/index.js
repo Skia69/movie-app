0c627ef5cb461716cb84b67ebc34717b7b33b42f
Skia69 <Skiastep@gmail.com>
2021-03-20 13:04:06 +0200

    feat: barrel export

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from "./Footer";
+export * from "./MovieGrid";
+export * from "./MovieItem";
+export * from "./Navbar";
+export * from "./Filter";
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export * from "./HomePage";
+export * from "./SearchPage";
+export * from "./MoviePage";
